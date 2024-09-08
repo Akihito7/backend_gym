71ab46f0d9f6344fcd5670f2acd72a3f1615db5d
akihito7 <akihitopro7@gmail.com>
2024-09-07 23:49:15 -0300

    fetch routine with exercises created

--- src/app/routines/routine.controller.ts
+++ src/app/routines/routine.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Param, ParseIntPipe, Req } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Param, ParseIntPipe, Req } from "@nestjs/common";
 import { RoutineService } from "./routine.service";
 
 @Controller('routine')
 export class RoutineController {
-  constructor(private readonly routineService : RoutineService){}
+  constructor(private readonly routineService: RoutineService) { }
 
   @Get('list/:id')
-  fetchListRoutines(@Param("id", ParseIntPipe) id : number) {
+  fetchListRoutines(@Param("id", ParseIntPipe) id: number) {
     return this.routineService.fetchListRoutines(id)
+  }
+
+  @Get(':routineId')
+  fetchRoutineWithExercises(@Param("routineId", ParseIntPipe) routineId: number) {
+    return this.routineService.fetchRoutineWithExercises(routineId)
   }
 }
--- src/app/routines/routine.service.ts
+++ src/app/routines/routine.service.ts
@@ -1,10 +1,52 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { pool } from "src/config/database";
 
 @Injectable()
 export class RoutineService {
-  async fetchListRoutines(id : number){
-    const response = await pool.query("SELECT * FROM routines WHERE user_id = $1", [id]);
-    return response.rows
+  async fetchListRoutines(id: number) {
+    try {
+      const response = await pool.query("SELECT * FROM routines WHERE user_id = $1", [id]);
+      return response.rows
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao buscar rotinas',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async fetchRoutineWithExercises(routineId: number) {
+    try {
+      const query = `
+      SELECT
+        r.id AS routine_id,
+        r.name AS routine_name,
+        r.description,
+        json_agg(
+          json_build_object(
+            'name', e.name,
+            'gif', e.gif
+          )
+        ) AS exercises
+      FROM
+        routines AS r
+      JOIN
+        routine_exercises AS re ON re.routine_id = r.id
+      JOIN
+        exercises AS e ON e.id = re.exercise_id
+      WHERE
+        r.user_id = 1
+        AND r.id = $1
+      GROUP BY
+        r.id, r.name, r.description;
+    `;
+    const result = await pool.query(query, [routineId]);
+    return result.rows[0];
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao buscar rotinas',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
